dec74b2e72145b711d97f3017ef3809cb10f9e7c
Leoltron <leoole5@gmail.com>
2020-02-23 00:33:20 +0500

    Cleanup

--- Dos.Utils/EnumerableExtensions.cs
+++ Dos.Utils/EnumerableExtensions.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace Dos.Utils
 {
@@ -18,39 +17,6 @@ namespace Dos.Utils
         public static IEnumerable<T> Times<T>(this IEnumerable<T> source, int repeatAmount)
         {
             return source.SelectMany(e => e.Repeat(repeatAmount));
-        }
-
-        public static void ShuffleFast<T>(this IList<T> list)
-        {
-            var n = list.Count;
-            while (n > 1)
-            {
-                n--;
-                var k = Rand.Next(n + 1);
-                var value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-
-        public static void Shuffle<T>(this IList<T> list)
-        {
-            var provider = new RNGCryptoServiceProvider();
-            var n = list.Count;
-            while (n > 1)
-            {
-                var box = new byte[1];
-                do
-                {
-                    provider.GetBytes(box);
-                } while (!(box[0] < n * (byte.MaxValue / n)));
-
-                var k = box[0] % n;
-                n--;
-                var value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
         }
 
         public static bool IsEmpty<T>(this IEnumerable<T> enumerable) => !enumerable.Any();
