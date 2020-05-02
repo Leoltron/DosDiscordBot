9c4172d33351e102ee300ebc5a2043354480854c
Leoltron <leoole5@gmail.com>
2020-05-03 00:51:12 +0500

    Updated version

--- Dos.DiscordBot/DosBot.cs
+++ Dos.DiscordBot/DosBot.cs
@@ -16,7 +16,7 @@ namespace Dos.DiscordBot
         public const string InviteLink = "https://discordapp.com/api/oauth2/authorize?client_id=660931324642590720&permissions=52224&scope=bot";
         public const string RepoLink = "https://github.com/Leoltron/DosDiscordBot";
 
-        private const string VersionStatus = "v1.2.4 (23.04.2020)";
+        private const string VersionStatus = "v1.2.5 (02.05.2020)";
 
         private readonly ILogger logger = new LoggerConfiguration()
                                          .WriteTo.Console(LogEventLevel.Information)
